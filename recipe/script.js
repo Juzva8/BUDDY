78e5000f8d9478aa80f8607b6b4a1bbd01bd5d0e
Juzva8 <72834046+Juzva8@users.noreply.github.com>
2020-11-12 14:09:54 -0800

    Add files via upload

--- recipe/script.js
+++ recipe/script.js
@@ -1,53 +1,29 @@
-// document.addEventListener("DOMContentLoaded", function() {
-
-// var appId = "bc65a9c5";
-// var appKey = "152ffcca6d5b1af41df34fc359228af2";
-// });
 let searchButton = document.querySelector("#search")
 
 
 searchButton.addEventListener("click", () => {
-    console.log("button pressed")
-        // sendApisRequest()
+    sendApisRequest()
 })
-
-// function forecast(searchTerm) {
-//     var queryURL = "https://api.edamam.com/search?app_id=${appId}&app_key=${appKey}&q=pizza"
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         for (var i = 0; i < response.list.length; i++)
-
-
 async function sendApisRequest() {
     let APP_ID = "bc65a9c5"
     let API_KEY = "152ffcca6d5b1af41df34fc359228af2"
-    let response = await fetch("https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=pizza");
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    useApiData(data)
+    let searchTerm = $("#searchText").val();
+    console.log(searchTerm);
+    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${searchTerm}`);
+    let data = await response.json();
+    console.log(data);
+    useApiData(data);
 }
 
 function useApiData(data) {
-    document.querySelector("#content").innerHTML = ` <
-    div class = "card col-4 offset-1"
-    style = "width: 18rem;" >
-        <
-        img src = "${data.hits[0].recipe.image}"
-    class = "card-img-top"
-    alt = "..." >
-        <
-        div class = "card-body" >
-        <
-        h5 class = "card-title" > ${hits[0].recipe.label} < /h5> <
-    p class = "card-text" > ${data.hits[0].recipe.calories}'.</p> <
-    a href = "${data.hits[0].recipe.url}"
-    class = "btn btn-primary" > Go somewhere < /a> < /
-    div > <
-        /div>
-`
-
+    for (let i = 0; i < 1; i++) {
+        let calories = data.hits[i].recipe.calories.toFixed(0)
+        $("#content").append(`<div class= "card col-4 offset-1" style = "width: 18rem;">
+            <img src = "${data.hits[i].recipe.image}" class = "card-img-top" alt = "...">        
+            <div class = "card-body">
+            <h5 class = "card-title"> ${data.hits[i].recipe.label} </h5> 
+            <p class="card-text">${calories}cal</p>
+        <a href = "${data.hits[i].recipe.url}" class="btn btn-primary"> get full recipe </a> </div > </div>`)
+        console.log(i);
+    }
 }
-// }

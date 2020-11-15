0c6976a80ee8015266f884393ff7659114df680f
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-11-15 10:10:16 -0800

    one more youtubeApi key

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,9 @@
 // behold: My Api Keys
 let weatherAPIKey = "8375474e12a3c07e327029469afe5cd7";
-let youtubeAPIKey = "AIzaSyAT2zEb0Dq2h6mvWjv2FwkhThxQ6oGewG0";
+// let youtubeAPIKey = "AIzaSyAT2zEb0Dq2h6mvWjv2FwkhThxQ6oGewG0";
 // let youtubeAPIKey = "AIzaSyB5YPkFZ4pig1XlwG-Wipon_eK2IGNIIH8";
 // let youtubeAPIKey = "AIzaSyC5_FFRE6UH2JrtDt6KPpr8qKVZ_f2VSjo";
+let youtubeAPIKey = "AIzaSyAX6zAlrvG1qIIhb0HCD7u32G5xEb2tvak";
 
 //declaring empty global variables for use in too many functions 
 let lat, lon, name, city, width, j, youtubeGet;
